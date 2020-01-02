a29d81836e120aa7327e9f1948c3bc2266e0ddb0
Matt Plichta <bigcatplichta@gmail.com>
2020-01-02 23:13:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let positionInt = dodger.style.left;
+    let position = parseInt(positionInt, 10);
+   
+    if (position > 0) {
+      dodger.style.left = `${position - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    let positionInt = dodger.style.left;
+    let position = parseInt(positionInt, 10);
+   
+    if (position < 360) {
+      dodger.style.left = `${position + 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+      }
+  });
